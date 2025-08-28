f25cd7f36a18a53669ab5666001a57f1600b185f
Sushil <sushilt2059@gmail.com>
2025-08-28 23:19:24 +0545

    connect

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -26,6 +26,7 @@ const deleteUserFromDB = inngest.createFunction(
     {id:"delete-user-from-db"},
     {event:"clerk/user.deleted"},
     async({event})=>{
+      await connectDB();
         const {id} = event.data;
         await User.deleteOne({clerkId: id});
       
