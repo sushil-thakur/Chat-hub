b45477e932fc6d26bf3eb1445f3a4109ec2efd41
Sushil <sushilt2059@gmail.com>
2025-08-29 21:37:41 +0545

    auth page implement

---
+++ frontend/pages/AuthPage.jsx
@@ -0,0 +1,54 @@
+import { SignInButton } from "@clerk/clerk-react";
+import React from "react";
+import "../src/styles/auth.css";
+const AuthPage = () => {
+  return (
+    <div className="auth-container">
+      <div className="auth-left">
+        <div className="auth-hero">
+          <div className="brand-container">
+            <img src="/logo.png" alt="slap" className="brand-logo" />
+            <span className="brand-name">Slap</span>
+          </div>
+          <h1 className="hero-title">Where Work Happens ✨</h1>
+          <p className="hero-subtitle">
+            Connect with your team instantly through secure, real-time
+            messaging. Experience seamless collaboration with powerful feature
+            designed fro modern teams.
+          </p>
+          <div className="features-list">
+            <div className="feature-item">
+              <span className="feature-icon">💬</span>
+              <span>Real-time messaging</span>
+            </div>
+
+            <div className="feature-item">
+              <span className="feature-icon">📽️</span>
+              <span>Video calls & meetings</span>
+            </div>
+
+            <div className="feature-item">
+              <span className="feature-icon">🔒</span>
+              <span>Secure & privates</span>
+            </div>
+          </div>
+
+          <SignInButton mode="modal">
+            <button className="cta-button">
+              GEt Started with Slap
+              <span className="button-arrow">→</span>
+            </button>
+          </SignInButton>
+        </div>
+      </div>
+      <div className="auth-right">
+        <div className="auth-image-container">
+          <img src="/auth-i.png" alt="Team collaboration" className="auth-image" />
+          <div className="image-overlay"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
-
+import React from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Route, Routes } from "react-router";
+import HomePage from "../pages/HomePage.jsx";
+import AuthPage from "../pages/AuthPage.jsx";
+import { Navigate } from "react-router";
 
 const App = () => {
   return (
     <div>
-      <header>
-      <SignedOut>
-        <SignInButton mode='modal' />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-    </div>
-  )
-}
+      <>
+        <SignedIn>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/auth" element={<Navigate to={"/"} replace />} />
+          </Routes>
+        </SignedIn>
+        <SignedOut>
+          <Routes>
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="*" element={<Navigate to={"/auth"} replace />} />
+          </Routes>
+        </SignedOut>
 
-export default App
+       
+      </>
+    </div>
+  );
+};
+
+export default App;
