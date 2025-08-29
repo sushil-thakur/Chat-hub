1efff433831396eaab2d322b1750d6778ec63436
Sushil <sushilt2059@gmail.com>
2025-08-29 14:41:50 +0545

    stream and sentry intergation

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "ChatterHub9" });
@@ -20,6 +21,11 @@ const syncUSer = inngest.createFunction(
     };
 
     await User.create(newUser);
+    await upsertStreamUser({
+      id:newUser.clerkId.toString(),
+      name: newUser.name,
+      image:newUser.image
+    })
   }
 );
 const deleteUserFromDB = inngest.createFunction(
@@ -29,6 +35,7 @@ const deleteUserFromDB = inngest.createFunction(
       await connectDB();
         const {id} = event.data;
         await User.deleteOne({clerkId: id});
+        await deleteStreamUser(id.toString());
       
     }
 )
---
+++ backend/src/config/stream.js
@@ -0,0 +1,31 @@
+import {StreamChat} from "stream-chat";
+import {ENV} from "../config/env.js"
+
+const streamClient = StreamChat.getInstance(ENV.STREAM_API_KEY, ENV.STREAM_API_SECRET)
+
+export const upsertStreamUser = async(userData)=>{
+    try {
+        await streamClient.upsertStreamUser(userData)
+        console.log("stream user upserted sucessfully:" ,userData.nmae)
+    } catch (error) {
+        console.log("error upserting stream user:",error)
+    }
+}
+export const deleteStreamUser = async (userId)=>{
+    try {
+        await streamClient.deactivateUser(userId)
+        console.log("user deleted sucesscfully:".userId)
+    } catch (error) {
+        console.error("error deleteding stream user:",error)
+    }
+}
+
+export const generateStreamToken = (userId)=>{
+    try {
+        const userIdString = userId.toString();
+        return streamClient.createToken(userIdString);
+    } catch (error) {
+        console.log("erroe generating stream token:", error);
+        return null;
+    }
+}
